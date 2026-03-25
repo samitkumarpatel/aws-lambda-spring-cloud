739329ba5cac132c60826a33e5944bbebc698584
samitkumarpatel <samitkumarpatel@gmail.com>
2026-03-25 23:14:12 +0100

    with multiple web endpoint support

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 - [Docs](https://docs.spring.io/spring-cloud-function/reference/adapters/aws-intro.html).
 - [Example](https://github.com/spring-cloud/spring-cloud-function/blob/main/spring-cloud-function-samples).
+
+### Web based spring cloud on aws lambda
+[Documentation to be followed] (https://docs.spring.io/spring-cloud-function/reference/adapters/aws-intro.html#serverless-java-container).
+
+- Deploy the generate jar during `./mvnw clean install`
+- After Deploy make sure to change the handler to `com.amazonaws.serverless.proxy.spring.SpringDelegatingLambdaContainerHandler`
+- Pass a Env variable `MAIN_CLASS=net.samitkumar.aws_lambda_spring_cloud.AwsLambdaSpringCloudApplication`
--- src/main/java/net/samitkumar/aws_lambda_spring_cloud/AwsLambdaSpringCloudApplication.java
+++ src/main/java/net/samitkumar/aws_lambda_spring_cloud/AwsLambdaSpringCloudApplication.java
@@ -3,8 +3,14 @@ package net.samitkumar.aws_lambda_spring_cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
-import java.util.function.Function;
+import java.util.Map;
+import java.util.Objects;
 
 @SpringBootApplication
 public class AwsLambdaSpringCloudApplication {
@@ -13,9 +19,26 @@ public class AwsLambdaSpringCloudApplication {
 		SpringApplication.run(AwsLambdaSpringCloudApplication.class, args);
 	}
 
+	//This does not work in lambda!
 	@Bean
-	public Function<String, String> uppercase() {
-		return value -> value.toUpperCase();
+	RouterFunction<ServerResponse> routerFunctions() {
+		return RouterFunctions
+				.route()
+				.GET("/functional/ping", request -> Mono.just(Map.of("pong","Hello Functional router")).flatMap(ServerResponse.ok()::bodyValue))
+				.build();
+	}
+}
+
+@RestController
+class WebController {
+
+	@GetMapping("/ping")
+	Map<String, String> ping() {
+		return Map.of("pong", "Hello World");
 	}
 
+	@PostMapping("/post")
+	Map<String, String> ping(@RequestBody String message) {
+		return Map.of("pong", Objects.nonNull(message) ? message : "Default Message");
+	}
 }
